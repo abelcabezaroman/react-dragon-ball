f4105bdb56b814766fd739a2032c519ce29afcea
acabezar <contacto@abelcabezaroman.com>
2024-04-05 17:10:17 +0200

    i

--- .history/src/App_20240405165513.jsx
+++ .history/src/App_20240405165513.jsx
@@ -1,16 +1,7 @@
-import { useEffect } from 'react'
 import './App.css'
 import Gallery from './components/Gallery/Gallery'
 
 function App() {
-
-  const getCharacters = async() => {
-
-  }
-
-  useEffect(() => {
-    getCharacters();
-  },[])
 
   return (
     <>
